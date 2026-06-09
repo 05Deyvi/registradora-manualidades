2c34ed616ed5f3d4d8bee5584924554470e1281c
05Deyvi <86032556+05Deyvi@users.noreply.github.com>
2026-06-09 13:26:07 -0500

    Add files via upload

---
+++ maual.py
@@ -0,0 +1,17 @@
+---------------------------------------------------------------------------
+ModuleNotFoundError                       Traceback (most recent call last)
+/tmp/ipykernel_21322/3033610658.py in <cell line: 0>()
+----> 1 import streamlit as st
+      2 
+      3 # Configuración de la página
+      4 st.set_page_config(page_title="Caja Registradora de Manualidades", page_icon="🎨", layout="centered")
+      5 
+
+ModuleNotFoundError: No module named 'streamlit'
+
+---------------------------------------------------------------------------
+NOTE: If your import is failing due to a missing package, you can
+manually install dependencies using either !pip or !apt.
+
+To view examples of installing some common dependencies, click the
+"Open Examples" button below.
